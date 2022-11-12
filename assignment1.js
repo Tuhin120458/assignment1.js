a52a1c27d633bbe35363b249c6c12faed342028a
Tuhin Islam <tuhin981172@gmail.com>
2022-11-12 12:24:03 +0600

    first commit

---
+++ assignment1.js
@@ -0,0 +1,20 @@
+// for concat(
+let basa = 'pabna';
+let now = 'dhaka';
+let basaNow = basa.concat(now);
+console.log(basaNow)
+
+//  every
+// let rolls = [12, 22, 33];
+// rolls.every(checkRoll)
+// function checkRoll(roll) {
+//     return roll > 10;
+// }
+
+// fill
+let arrey = [2, 3, 4]
+console.log(arrey.fill(6, 1, 5));
+
+
+
+
